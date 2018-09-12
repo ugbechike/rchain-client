b6b47719d4a227e02d591c82f5f5c66ed4e57177
Ojini Chizoba Jude <judoc96@gmail.com>
2018-09-12 18:06:26 +0100

    add navigation menu to signin and signup page

--- rchain/src/Components/container.js
+++ rchain/src/Components/container.js
@@ -14,9 +14,9 @@ class Container extends Component {
 			<div>
 				<Responsive>
 					<Switch>
-            <Route path="/signup" component={SignUp} exact />
-            <Route path="/login" component={SignIn} exact/>
-            <Route path="/reset" component={ForgotPassword} exact/>
+			            <Route path="/signup" component={SignUp} exact />
+			            <Route path="/login" component={SignIn} exact/>
+			            <Route path="/reset" component={ForgotPassword} exact/>
 						<Route path="/" component={Home} />
 						<Route component={NotFound} />
 
